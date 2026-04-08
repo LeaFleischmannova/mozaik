84542f9d35b2f3c79400a951dca60a1f95e7ecd2
Lea Fleischmannová <lea.fleischmannova@gmail.cz>
2026-04-08 17:59:56 +0200

    Add random video generator

--- tests/full_model/models/LSV1M_tiny_opto/random_video/random_video.py
+++ tests/full_model/models/LSV1M_tiny_opto/random_video/random_video.py
@@ -1,17 +1,18 @@
 import numpy as np
 import os
 
-x = 16
-y = 16
+x = 400
+y = 400
 duration = 1000
-update_interval = 100
+update_interval = 1
 n_frames = int(duration / update_interval)
 
 np.random.seed(0)
-video = np.random.rand(x, y, n_frames).astype(np.float32)
+video = np.random.rand(n_frames, x, y).astype(np.float32)
 
 folder = os.path.dirname(__file__)
 file_name = "random_opto_video_1s.npy"
 path = os.path.join(folder, file_name)
 
 np.save(path, video)
+print(video)
